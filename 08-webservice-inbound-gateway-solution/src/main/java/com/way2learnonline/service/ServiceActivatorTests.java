9e39d01a5e6d4402692e761ae056077782c16f68
vinayakbaddi <vinayakbaddi@gmail.com>
2017-07-07 16:47:47 +0530

    WOrking on Spring JMS without much configuration

--- 08-webservice-inbound-gateway-solution/src/main/java/com/way2learnonline/service/ServiceActivatorTests.java
+++ 08-webservice-inbound-gateway-solution/src/main/java/com/way2learnonline/service/ServiceActivatorTests.java
@@ -2,14 +2,19 @@ package com.way2learnonline.service;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
 
+import com.way2learnonline.domain.Order;
+import com.way2learnonline.domain.ProcessOrderRequest;
 
 
 public class ServiceActivatorTests {
 	
 	public static void main(String[] args) {
-		ApplicationContext context= new ClassPathXmlApplicationContext("integration-beans.xml");
-		/*MessageChannel messageChannel=context.getBean("orders-channel",MessageChannel.class);
+		ApplicationContext context= new ClassPathXmlApplicationContext("inbound-gateway-config.xml");
+		MessageChannel messageChannel=context.getBean("orders-channel",MessageChannel.class);
 		
 		Order order= new Order();
 		order.setCustomerID("1111");
@@ -24,7 +29,7 @@ public class ServiceActivatorTests {
 		Message<ProcessOrderRequest> orderMessage= MessageBuilder.withPayload(request).build();
 		
 		
-		messageChannel.send(orderMessage);*/
+		messageChannel.send(orderMessage);
 		//messageChannel.send(orderMessage);
 		//messageChannel.send(orderMessage);
 		
